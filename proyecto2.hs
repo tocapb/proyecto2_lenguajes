955c29bdec635171838a1c53c0ac9c320c5f31ee
tocapb <jeremy.mp1512@gmail.com>
2020-10-24 10:35:36 -0600

    se agrega funcionalidad no terminada de parqueos y un archivo txt para probarlo

--- proyecto2.hs
+++ proyecto2.hs
@@ -1,36 +1,48 @@
---Entradas:Ninguna
+-- datos de la empresa
+-- guardados como globales
+nombre_empresa = "Los cleteros"
+sitio_web = "www.cletaslocas.com"
+contacto = 87456987
+tarifa_pedal = 1000
+tarifa_electronico = 2000
+
 --Salidas:Ninguna
 --Funcionalidad:Menú inicial introduciendo el usuario que permite acceder a las funcionalidades
 inicio:: IO()
 inicio= do
+    putStr("Introduzca la ruta del archivo para cargar los parqueos:")
+    ruta <- getLine
+    parqueos <- leerParqueos ruta
+    print("Datos de parqueos cargados")
     putStr("\nBienvenido\n")
     putStr("Introduzca el usuario:")
     usuario <- getLine
     case usuario of
-        "admin" -> menu_principal
+        "admin" -> menu_principal(-1,parqueos)
         
     return()
---Entradas:Ninguna
+--Entradas:Una opcion de tipo entero y una lista de tipo Parqueo
 --Salidas:Ninguna
 --Funcionalidad:Menú principal para acceder a las opciones operativos o generales
-menu_principal:: IO()
-menu_principal= do
+menu_principal(opcion, parqueos)= do
     putStr("\nMenú Principal\n")
+
     putStr("1.Opciones Operativas\n")
     putStr("2.Opciones Generales \n")
     putStr("3.Salir \n")
     putStr("Introduzca su opción:")
-    opcion <- getLine
+    temporal <- getLine
+    let opcion = (read temporal :: Integer)
     case opcion of
-        "1" -> menu_operativo
-        "2" -> menu_general
-        "3" -> return()
+        -1-> print("")
+        1 -> menu_operativo(-1,parqueos)
+        2 -> menu_general(-1,parqueos)
+        3 -> return()
         
---Entradas:Ninguna
+--Entradas:Una opcion de tipo entero y una lista de tipo Parqueo
 --Salidas:Ninguna
 --Funcionalidad:Menú operativo para acceder a las funciones operativas
-menu_operativo:: IO()
-menu_operativo= do
+menu_operativo(opcion, parqueos)= do
     putStr("\nMenú Operativo\n")
     putStr("1.Mostrar parqueos\n")
     putStr("2.Mostrar bicicletas \n")
@@ -38,20 +50,21 @@ menu_operativo= do
     putStr("4.Estadísticas \n")
     putStr("5.Volver \n")
     putStr("Introduzca su opción:")
-    opcion <- getLine
+    temporal <- getLine
+    let opcion = (read temporal :: Integer)
     case opcion of
-        "1" -> putStr("1")
-        "2" -> putStr("2")
-        "3" -> putStr("3")
-        "4" -> putStr("4")
-        "5" -> menu_principal
+        -1 -> print("")
+        1 -> showParqueos parqueos
+        2 -> putStr("2")
+        3 -> putStr("3")
+        4 -> putStr("4")
+        5 -> menu_principal(-1,parqueos)
     return()
 
---Entradas:Ninguna
+--Entradas:Una opcion de tipo entero y una lista de tipo Parqueo
 --Salidas:Ninguna
 --Funcionalidad:Menú principal para acceder a las funciones generales   
-menu_general:: IO()
-menu_general= do
+menu_general(opcion,parqueos)= do
     putStr("\nMenú Generales\n")
     putStr("1.Consultar bicicletas\n")
     putStr("2.Alquiler\n")
@@ -59,11 +72,99 @@ menu_general= do
     putStr("4.Consulta de factura \n")
     putStr("5.Volver \n")
     putStr("Introduzca su opción:")
-    opcion <- getLine
+    temporal <- getLine
+    let opcion = (read temporal :: Integer)
     case opcion of
-        "1" -> putStr("1")
-        "2" -> putStr("2")
-        "3" -> putStr("3")
-        "4" -> putStr("4")
-        "5" -> menu_principal
+        -1 -> print("")
+        1 -> putStr("1")
+        2 -> putStr("2")
+        3 -> putStr("3")
+        4 -> putStr("4")
+        5 -> menu_principal(-1,parqueos)
     return()
+
+--estructura para almacenar parqueos--------------------------------------------------
+type Nombre_parqueo = String
+type Direccion_parqueo = String
+type Provincia = String
+type X_parqueo = Integer
+type Y_parqueo = Integer
+data Parqueo = Parqueo Nombre_parqueo Direccion_parqueo Provincia X_parqueo Y_parqueo;
+--------------------------------------------------------------------------------------
+
+-- Constructor de parqueos---------------------------------------------------------------------------------------------------------------
+creaParqueo(elemento) = Parqueo (elemento!!0) (elemento!!1) (elemento!!2) (read (elemento!!3) :: Integer) (read (elemento!!4) :: Integer)
+getNombre_parqueo (Parqueo nombre_parqueo _ _ _ _) = nombre_parqueo;
+getDireccion_parqueo (Parqueo _ direccion_parqueo _ _ _) = direccion_parqueo;
+getProvincia (Parqueo _ _ provincia _ _) = provincia;
+getX_parqueo (Parqueo _ _ _ x _) = x;
+getY_parqueo (Parqueo _ _ _ _ y) = y;
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+--Entradas:Una ruta de archivo
+--Salidas:Una lista de parqueos
+--Funcionalidad:Guarda el contenido del archivo
+leerParqueos :: FilePath -> IO [Parqueo]
+leerParqueos archivo = do
+    contenido <- readFile archivo
+    let parqueos = separaElementos (lines contenido)
+    return parqueos
+
+--Entradas:Una lista de listas de Char
+--Salidas:Una lista de Parqueos
+--Funcionalidad:se encarga de ir almacenando los parqueos ya bien estructurados
+separaElementos :: [[Char]]-> [Parqueo]
+separaElementos lista = 
+    if null(lista) then []
+    else
+        [creaParqueo(separaPorComas((head lista),""))] ++ separaElementos (tail lista)
+
+ --Entradas:Una tupla de listas de char
+--Salidas:Una lista de listas de char
+--Funcionalidad:se encarga de separar por comas el contenido enviado
+separaPorComas :: ([Char],[Char])->[[Char]]
+separaPorComas (cadena, temp) =
+    if cadena == "" then [temp]
+    else
+        if (head cadena) == (head ",") then
+            [temp] ++ separaPorComas ((tail cadena), "")
+        else
+            separaPorComas ((tail cadena), temp++[(head cadena)])
+
+--Entradas:Una lista de parqueos
+--Salidas:No tiene
+--Funcionalidad:se encarga pedir la provincia por buscar en los parqueos
+showParqueos :: [Parqueo] -> IO()
+showParqueos lista = do
+    putStr("Introduzca la provincia: ")
+    temporal <- getLine
+    showParqueosAux lista temporal
+    menu_operativo(-1,lista)
+
+--Entradas:Una lista de parqueos y un string
+--Salidas:No tiene
+--Funcionalidad:se encarga de validar cuando se acaban los parqueos cargados
+showParqueosAux :: [Parqueo] -> String -> IO ()
+showParqueosAux [ ] prov = print("")
+showParqueosAux lista prov=
+    
+    do  
+            showParqueo (head lista) prov
+            showParqueosAux (tail lista) prov
+
+--Entradas:Un parqueo y un string
+--Salidas:No tiene
+--Funcionalidad:se encarga de imprimir la informacion del parqueo
+showParqueo :: Parqueo -> String -> IO ()
+showParqueo parqueo prov=
+    let 
+        nombre_parqueo = getNombre_parqueo(parqueo)
+        direccion_parqueo = getDireccion_parqueo(parqueo)
+        provincia = getProvincia(parqueo)
+        x = getX_parqueo(parqueo)
+        y = getY_parqueo(parqueo)
+    in
+        if provincia == prov then
+            print("nombre: " ++ nombre_parqueo ++ ", direccion: " ++ direccion_parqueo ++ ", provincia: " ++ provincia ++ ", x: " ++ show x ++ ", y: " ++ show y)
+        else
+            return ()
