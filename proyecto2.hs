d36f104085655a89dfd538d5464b245fe0c2c233
randox19 <randoxyt@gmail.com>
2020-10-24 13:23:54 -0600

    Agrego la funcionalidad de cargar y mostrar usuarios, también agrego el archivo que almacena usuarios

--- proyecto2.hs
+++ proyecto2.hs
@@ -14,17 +14,21 @@ inicio= do
     ruta <- getLine
     parqueos <- leerParqueos ruta
     print("Datos de parqueos cargados")
+    putStr("Introduzca la ruta del archivo para cargar los usuarios:")
+    rutaUser <- getLine
+    usuarios <- leerUsuarios rutaUser
+    print("Datos de usuarios cargados")
     putStr("\nBienvenido\n")
     putStr("Introduzca el usuario:")
     usuario <- getLine
     case usuario of
-        "admin" -> menu_principal(-1,parqueos)
+        "admin" -> menu_principal(-1,parqueos,usuarios)
         
     return()
 --Entradas:Una opcion de tipo entero y una lista de tipo Parqueo
 --Salidas:Ninguna
 --Funcionalidad:Menú principal para acceder a las opciones operativos o generales
-menu_principal(opcion, parqueos)= do
+menu_principal(opcion, parqueos,usuarios)= do
     putStr("\nMenú Principal\n")
 
     putStr("1.Opciones Operativas\n")
@@ -35,14 +39,14 @@ menu_principal(opcion, parqueos)= do
     let opcion = (read temporal :: Integer)
     case opcion of
         -1-> print("")
-        1 -> menu_operativo(-1,parqueos)
-        2 -> menu_general(-1,parqueos)
+        1 -> menu_operativo(-1,parqueos,usuarios)
+        2 -> menu_general(-1,parqueos,usuarios)
         3 -> return()
         
 --Entradas:Una opcion de tipo entero y una lista de tipo Parqueo
 --Salidas:Ninguna
 --Funcionalidad:Menú operativo para acceder a las funciones operativas
-menu_operativo(opcion, parqueos)= do
+menu_operativo(opcion, parqueos,usuarios)= do
     putStr("\nMenú Operativo\n")
     putStr("1.Mostrar parqueos\n")
     putStr("2.Mostrar bicicletas \n")
@@ -54,17 +58,17 @@ menu_operativo(opcion, parqueos)= do
     let opcion = (read temporal :: Integer)
     case opcion of
         -1 -> print("")
-        1 -> showParqueos parqueos
+        1 -> showParqueos parqueos usuarios
         2 -> putStr("2")
-        3 -> putStr("3")
+        3 -> showUsuarios usuarios parqueos
         4 -> putStr("4")
-        5 -> menu_principal(-1,parqueos)
+        5 -> menu_principal(-1,parqueos,usuarios)
     return()
 
 --Entradas:Una opcion de tipo entero y una lista de tipo Parqueo
 --Salidas:Ninguna
 --Funcionalidad:Menú principal para acceder a las funciones generales   
-menu_general(opcion,parqueos)= do
+menu_general(opcion,parqueos,usuarios)= do
     putStr("\nMenú Generales\n")
     putStr("1.Consultar bicicletas\n")
     putStr("2.Alquiler\n")
@@ -80,7 +84,7 @@ menu_general(opcion,parqueos)= do
         2 -> putStr("2")
         3 -> putStr("3")
         4 -> putStr("4")
-        5 -> menu_principal(-1,parqueos)
+        5 -> menu_principal(-1,parqueos,usuarios)
     return()
 
 --estructura para almacenar parqueos--------------------------------------------------
@@ -134,12 +138,12 @@ separaPorComas (cadena, temp) =
 --Entradas:Una lista de parqueos
 --Salidas:No tiene
 --Funcionalidad:se encarga pedir la provincia por buscar en los parqueos
-showParqueos :: [Parqueo] -> IO()
-showParqueos lista = do
+showParqueos :: [Parqueo]->[Usuario] -> IO()
+showParqueos lista usuarios = do
     putStr("Introduzca la provincia: ")
     temporal <- getLine
     showParqueosAux lista temporal
-    menu_operativo(-1,lista)
+    menu_operativo(-1,lista,usuarios)
 
 --Entradas:Una lista de parqueos y un string
 --Salidas:No tiene
@@ -168,3 +172,86 @@ showParqueo parqueo prov=
             print("nombre: " ++ nombre_parqueo ++ ", direccion: " ++ direccion_parqueo ++ ", provincia: " ++ provincia ++ ", x: " ++ show x ++ ", y: " ++ show y)
         else
             return ()
+
+--estructura para almacenar usuarios--------------------------------------------------
+type Cedula = Integer
+type Nombre_usuario = String
+data Usuario = Usuario Cedula Nombre_usuario  ;
+--------------------------------------------------------------------------------------
+
+-- Constructor de usuario---------------------------------------------------------------------------------------------------------------
+creaUsuario(elemento) = Usuario (read (elemento!!0) :: Integer) (elemento!!1) 
+getCedula (Usuario cedula _ ) = cedula;
+getNombre_usuario (Usuario _ nombre_usuario ) = nombre_usuario;
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+--Entradas:Una ruta de archivo
+--Salidas:Una lista de usuarios
+--Funcionalidad:Guarda el contenido del usuario
+leerUsuarios :: FilePath -> IO [Usuario]
+leerUsuarios archivo = do
+    contenido <- readFile archivo
+    let usuarios = separaElementosUsuario (lines contenido)
+    return usuarios
+
+--Entradas:Una lista de listas de Char
+--Salidas:Una lista de Parqueos
+--Funcionalidad:se encarga de ir almacenando los parqueos ya bien estructurados
+separaElementosUsuario :: [[Char]]-> [Usuario]
+separaElementosUsuario lista = 
+    if null(lista) then []
+    else
+        [creaUsuario(separaPorComas((head lista),""))] ++ separaElementosUsuario (tail lista)
+
+showUsuarios :: [Usuario]->[Parqueo] -> IO()
+showUsuarios lista parqueos = do
+    putStr("Introduzca el número de cedula: ")
+    temporal <- getLine
+    showUsuariosAux lista temporal
+    menu_operativo(-1,parqueos,lista)
+
+--Entradas:Una lista de usuarios y un string
+--Salidas:No tiene
+--Funcionalidad:se encarga de validar cuando se acaban los usuarios cargados
+showUsuariosAux :: [Usuario] -> String -> IO ()
+showUsuariosAux [ ] cedul = print("")
+showUsuariosAux lista cedul=
+    
+    do  
+        let cedul_int=(read (cedul) :: Integer)
+        
+        if "#" == cedul then
+            do
+            showTodosUsuarios (head lista) 
+            showUsuariosAux (tail lista) cedul
+        else 
+            do   
+            showUsuario (head lista) cedul_int
+            showUsuariosAux (tail lista) cedul
+
+--Entradas:Un usuarios y un string
+--Salidas:No tiene
+--Funcionalidad:se encarga de imprimir la informacion de los usuarios
+showUsuario :: Usuario -> Integer -> IO ()
+showUsuario usuario cedul=
+    let 
+        cedula = getCedula(usuario)
+        nombre_usuario = getNombre_usuario(usuario)
+    in
+        if cedula == cedul then
+            print("Cedula: " ++ show cedula ++ ", Nombre del Usuario: " ++ nombre_usuario)
+        else
+            return ()
+--Entradas:Un usuarios
+--Salidas:No tiene
+--Funcionalidad:se encarga de imprimir la informacion de todos los usuarios
+showTodosUsuarios :: Usuario -> IO ()
+showTodosUsuarios usuario =
+    let 
+        cedula = getCedula(usuario)
+        nombre_usuario = getNombre_usuario(usuario)
+    in
+
+        print("Cedula: " ++ show cedula ++ ", Nombre del Usuario: " ++ nombre_usuario)
+        
+            
